be5ae30041d861dec621b070412a49ba08fd81c2
Alex Alexeyev <aasmc@yandex.ru>
2024-05-13 12:13:50 +0300

    add realm import for keycloak

---
+++ get-access-token-user-max.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+curl -X POST http://localhost:8080/realms/cloud-java/protocol/openid-connect/token -d 'username=max' -d 'password=password' -d 'grant_type=password' -d 'client_id=cloud-java-gateway' -d 'client_secret=z8DEMatUnmNTJkPk1ov8Cc8nXYzpLdaD'
