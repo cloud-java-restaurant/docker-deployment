05f08f4a158fca60bf37ecdb5db5e93392d21e4d
Alex Alexeyev <aasmc@yandex.ru>
2024-05-28 22:59:30 +0300

    remove unnecessary scripts

--- get-access-token-admin-alex.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-curl -X POST http://keycloak:8080/realms/cloud-java/protocol/openid-connect/token -d 'username=alex' -d 'password=password' -d 'grant_type=password' -d 'client_id=cloud-java-gateway' -d 'client_secret=z8DEMatUnmNTJkPk1ov8Cc8nXYzpLdaD' -d 'scope=openid'
