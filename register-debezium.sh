66f30f7fe924f449f4bbcb28f6f372b8194c7d75
aleksas <aasmc@yandex.ru>
2024-10-30 20:22:02 +0300

    fix curl register debezium

--- register-debezium.sh
+++ register-debezium.sh
@@ -2,4 +2,4 @@
 
 echo -e "\n*** Registering Debezium Config ***\n"
 
-curl -s -S -XPOST -H Accept:application/json -H Content-Type:application/json http://localhost:8083/connectors/ -d @debezium/debezium-config.json
+curl -X POST -H Accept:application/json -H Content-Type:application/json http://localhost:8083/connectors/ -d @debezium/debezium-config.json
